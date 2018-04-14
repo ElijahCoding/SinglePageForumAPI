e4de8095589a351ff66a0555d0b52ef5f02ba379
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-15 02:29:15 +0300

    Signing in with JWT authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,4 +8,10 @@ Route::group(['middleware' => ['api']], function() {
   Route::post('/auth/signin', [
     'uses' => 'AuthController@signIn'
   ]);
+
+  Route::group(['middleware' => 'jwt.auth'], function() {
+    Route::get('/test', function() {
+      dd('You are authenticated');
+    });
+  });
 });
