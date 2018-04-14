ffc2ac6cd8eec569a254a09a3d920b01b724d9a9
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-15 02:22:41 +0300

    sign in method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use JWTAuth;
 use Illuminate\Http\Request;
-
 use App\Models\User;
 use App\Http\Requests;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use App\Http\Requests\Auth\RegisterFormRequest;
 
 class AuthController extends Controller
@@ -18,4 +19,24 @@ class AuthController extends Controller
         'password' => bcrypt($request->json('password')),
       ]);
     }
+
+    public function signIn(Request $request)
+    {
+      try {
+        $token = JWTAuth::attempt($request->only('email', 'password'));
+      } catch (JWTException $e) {
+        return response()->json([
+          'error' => 'Could not autenticate.'
+        ], 500);
+      }
+
+    if (!$token) {
+      return response()->json([
+        'error' => 'Could not autenticate.'
+      ], 401);
+    }
+
+    return response()->json(compact('token'));
+  }
+
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,4 +4,8 @@ Route::group(['middleware' => ['api']], function() {
   Route::post('/auth/signup', [
     'uses' => 'AuthController@signUp'
   ]);
+
+  Route::post('/auth/signin', [
+    'uses' => 'AuthController@signIn'
+  ]);
 });
